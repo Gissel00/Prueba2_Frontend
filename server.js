5228088402e566346f2dcf90e0473a24a25ac7db
Gissel <Gissel55sp@gmail.com>
2021-12-06 22:18:38 -0600

    actualizaciones para implementar en heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-playlist>'));
+app.use(express.static(__dirname + '/dist/app-playlist'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-playlist>/index.html'));});
+'/dist/app-playlist/index.html'));});
 app.listen(process.env.PORT || 8080);
